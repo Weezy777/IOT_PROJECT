1919b77963648c3cec9c31b490e8ce735e423ac7
Gawayne Wright <gawaynewright10@outlook.com>
2023-05-20 06:20:58 -0500

    FIXED ORIGNS

--- api/app.py
+++ api/app.py
@@ -15,7 +15,8 @@ app = FastAPI()
 
 origins = [
     "https://simple-smart-hub-client.netlify.app",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://gw-ecse3038-iot-project.onrender.com"
 
 ]
 
